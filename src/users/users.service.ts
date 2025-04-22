0e518804f7fd237e6b758f7f6e6e11e3436b7636
silvante <khamidov.ko@gmail.com>
2025-04-22 21:49:00 +0500

    connecting prisma to user resource

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
+
+  constructor(private readonly prisma: PrismaService) {}
   
   findAll() {
-    return `This action returns all users`;
+    return this.prisma.user.findMany();
   }
 
   findOne(id: number) {
