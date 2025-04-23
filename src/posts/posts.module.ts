cbbfc519256a057d9821e91188f693b44388c288
Muhammadali-fr <boburjonr934@gmail.com>
2025-04-23 21:06:27 +0500

    posts

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
-import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
   controllers: [PostsController],
   providers: [PostsService],
 })
